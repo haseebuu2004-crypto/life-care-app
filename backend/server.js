c00f7ee7c344852a175070b29631fc01d505ba53
haseeb <haseebuu2004@gmail.com>
2026-05-20 20:50:44 +0530

    fixed frontend path

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 
 // Serve Static Frontend for production / multi-container 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // Fallback error handling
 app.use('/api', (req, res) => {
@@ -26,13 +26,13 @@ app.use('/api', (req, res) => {
 
 // React Router Fallback
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
 // We export `app` for testing purposes
 if (require.main === module) {
     app.listen(PORT, () => {
-        console.log(`High-performance Node server running on port ${PORT}`);
+        console.log(`server running on port ${PORT}`);
     });
 }
 
